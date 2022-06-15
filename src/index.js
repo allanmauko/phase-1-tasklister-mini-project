c26cb60a928f7097fa8bee100ed89106a4f53ad2
allanmauko <allan.mauko@student.moringaschool.com>
2022-06-15 10:11:45 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () => {
+    let form = document.querySelector('form')
+    form.addEventListener('submit',(e) => {
+    e.preventDefault()
+    handleMyToDo(e.target.new-task-description.value)
+  })
+})
+
+function handleMyToDo(mytodo){
+let p = document.createElement('p')
+let btn = document.createElement('button')
+btn.addEventListener('click', handleDelete)
+btn.textContent = 'x'
+p.textContent = `${mytodo}`
+p.appendChild(btn)
+console.log(p)
+document.querySelector('#mytodo_container').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
